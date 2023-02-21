427ca83aa9c4a7ad8d6ffbacc1c2bd147a07f27a
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-02-21 10:32:23 -0500

    List the name of all animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT *
+FROM animals
+WHERE name LIKE '%mon';
+SELECT name
+FROM animals
+WHERE date_of_birth BETWEEN 'Jan 01,2016' AND 'Dec 31, 2019';
+
