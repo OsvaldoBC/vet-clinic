1ca6d2db4632b33a2fdb8d890ae1a3ed8fb23ae7
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-02-21 17:27:32 -0500

    inside a transaction update the animals table and commit

--- queries.sql
+++ queries.sql
@@ -33,3 +33,13 @@ UPDATE animals
 SET species = 'unspecified';
 ROLLBACK;
 
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+
