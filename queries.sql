04aea512d6a8fcf3c79727930db53fc75dc4cf66
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-02-21 10:34:19 -0500

    List the name of all animals that are neutered and have less than 3 escape attempts

--- queries.sql
+++ queries.sql
@@ -6,4 +6,8 @@ WHERE name LIKE '%mon';
 SELECT name
 FROM animals
 WHERE date_of_birth BETWEEN 'Jan 01,2016' AND 'Dec 31, 2019';
-
+SELECT name
+FROM animals
+WHERE neutered = true
+    AND escape_attempts < 3;
+    
