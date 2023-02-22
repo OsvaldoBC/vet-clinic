374e38ddc76913b4aede6264e02b80f9f5600f3b
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-02-22 12:17:48 -0500

    insert data in owners table

--- data.sql
+++ data.sql
@@ -73,3 +73,16 @@ INSERT INTO animals (
     )
 
 VALUES ('Ditto', 'May 14, 2022', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
